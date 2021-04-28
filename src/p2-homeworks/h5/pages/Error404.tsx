38a3c9d3fa44e7c21c68632abaded1ea80e6e06e
marmon179 <marmon179@gmail.com>
2021-04-28 11:24:09 +0300

    5 homework

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import s from './Error404.module.css'
 
 function Error404() {
+    const eyeRit = `${s.eye} ${s.eyeR}`
+    const blodAll = `${s.blod} ${s.blod2}`
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.container}>
+
+            <div className={s.error}>
+                <p className={s.p}>4</p>
+                <span className={s.dracula}>
+			<div className={s.con}>
+				<div className={s.hair}/>
+				<div className={s.hairR}/>
+				<div className={s.head}/>
+    		<div className={s.eye}/>
+    		<div className={eyeRit}/>
+  			<div className={s.mouth}/>
+  			<div className={s.blod}/>
+  			<div className={blodAll}/>
+			</div>
+		</span>
+                <p className={s.p}>4</p>
+
+                <div className={s.pageMs}>
+                    <p className={s.pageMsg}> Oops, the page you're looking for Disappeared </p>
+                    {/*<button className={s.goBack} >Go Back</button>*/}
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5/pages/Junior+.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function JuniorPlus() {
+    return (
+        <div>
+            JUNIOR+
+        </div>
+    )
+}
+
+export default JuniorPlus
