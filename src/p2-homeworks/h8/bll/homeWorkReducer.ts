dba6aa7b8dc1f5942d62cd0f4ebb9d39d984fa7c
marmon179 <marmon179@gmail.com>
2021-05-07 18:48:36 +0300

    8 Finish

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,39 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from '../HW8';
+
+type SortPeopleActionType = {
+    type: 'sort'
+    payload: 'up' | 'down'
+
+}
+type CheckActionType = {
+    type: 'check'
+    payload: number
+}
+
+type ActionType = SortPeopleActionType | CheckActionType
+
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            const newState = [...state].sort((a, b) => {
+                if (a.name > b.name) return 1
+                else if (a.name < b.name) return -1
+                else return 0
+            })
+            return action.payload === 'up' ? newState : newState.reverse()
         }
         case 'check': {
-            // need to fix
-            return state
+
+            return state.filter(t => t.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
+}
+export const sortPeopleAC = (payload:'up' | 'down'):SortPeopleActionType => {
+    return {type: 'sort', payload}
+}
+export const checkPeopleAC = (payload:number):CheckActionType => {
+    return {type: 'check', payload}
 }
