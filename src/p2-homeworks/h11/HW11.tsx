0b4558b4610c64ac2308ef8fc38631edcd6b991d
marmon179 <marmon179@gmail.com>
2021-05-14 19:59:34 +0300

    11 refactoring

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -5,37 +5,40 @@ import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange';
 
 function HW11() {
     const [value1, setValue1] = useState<number>(10)
-    const [value2, setValue2] = useState<number[]>([34, 54])
+    const [value2, setValue2] = useState<number>(50)
+    const onChangeRange = (value: number | number[]) => {
+        if (Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        }
+    }
 
-    const handleChange = (event: ChangeEvent<{}>, newValue: any) => setValue1(newValue)
-    const handleChange2 = (event: ChangeEvent<{}>, newValue: any) => setValue2(newValue)
+    // const handleChange = (event: ChangeEvent<{}>, newValue: any) => setValue1(newValue)
+    // const handleChange2 = (event: ChangeEvent<{}>, newValue: any) => setValue2(newValue)
 
     return (
         <div>
             <hr/>
             homeworks 11
-
             {/*should work (должно работать)*/}
             <div className={s.body}>
                 <div>
-
                     <span>{value1}</span>
-
                     <SuperRange
-                        handleChange={handleChange}
-                        value1={value1}
+                        value={value1}
+                        onChangeRange={setValue1}
+                        // сделать так чтоб value1 изменялось
                     />
-
-
                 </div>
 
                 <div>
-                    <span>{value2[0]}</span>
+                    <span>{value1}</span>
                     <SuperDoubleRange
-                        handleChange2={handleChange2}
-                        value2={value2}
+                        // сделать так чтоб value1 и value2 изменялось
+                        value={[value1, value2]}
+                        onChangeRange={onChangeRange}
                     />
-                    <span>{value2[1]}</span>
+                    <span>{value2}</span>
                 </div>
             </div>
 
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,31 +1,43 @@
-import React, {ChangeEvent} from 'react'
-import {makeStyles, Slider} from '@material-ui/core';
+import React from 'react';
+import Slider from '@material-ui/core/Slider';
+
+
+// тип пропсов обычного инпута
+
 
 type SuperDoubleRangePropsType = {
-    handleChange2: (event: ChangeEvent<{}>, newValue: any) => void
-    value2: number[]
-
+    onChangeRange?: (value:number | number[]) => void
+    value: [number, number]
+    onChangeRangeO?: (value:number) => void
+    // min, max, step, disable, ...
 }
 
+function valuetext(value: number) {
+    return `${value}`;
+}
 
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = ({value2, handleChange2}) => {
-
-    const useStyles = makeStyles({
-        root: {
-            width: 300,
-        },
-    });
-    const classes = useStyles();
+const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
+    {
+        onChangeRange, value,
+    }
+) => {
+    // сделать самому, можно подключать библиотеки
+    const handleChange = (event: React.ChangeEvent<{}>, value: (number | number[])) => {
+        onChangeRange && onChangeRange(value as number)
+    };
     return (
-        <div className={classes.root}>
-            <Slider
-                onChange={handleChange2}
-                value={value2}
-                valueLabelDisplay="auto"
-                aria-labelledby="range-slider"
-            />
-        </div>
-    )
+        <Slider
+            style={{
+                color:"grey",
+                width:500
+            }}
+            value={value}
+            onChange={handleChange}
+            valueLabelDisplay="auto"
+            aria-labelledby="range-slider"
+            getAriaValueText={valuetext}
+        />
+    );
 }
 
-export default SuperDoubleRange
+export default SuperDoubleRange;
